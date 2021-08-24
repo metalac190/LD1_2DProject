aa303017b5e5375c9ec35dd208404d103b074b6e
Adam Chandler <achaysmail@gmail.com>
2021-08-24 16:33:08 -0500

    Teleport Particle

--- Assets/_Game/Scripts/LevelMechanics/Hazards/TeleportVolume.cs
+++ Assets/_Game/Scripts/LevelMechanics/Hazards/TeleportVolume.cs
@@ -7,13 +7,23 @@ public class TeleportVolume : TriggerVolume
     [Header("Teleport Settings")]
     [SerializeField]
     private Transform _exitTransform;
+    [SerializeField]
+    private ParticleSystem _exitParticles;
 
     public override void TriggerEntered(Collider2D otherCollider)
     {
         MovementKM movement = otherCollider.GetComponent<MovementKM>();
         if(movement != null)
         {
+            if(_exitParticles != null)
+            {
+                ParticleSystem exitParticles = Instantiate
+                    (_exitParticles, _exitTransform.position, Quaternion.identity);
+                exitParticles.Play();
+            }
+
             movement.MovePositionInstant(_exitTransform.position);
+            
         }
     }
 }
